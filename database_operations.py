6f93382509aa86244a3bb08a10bc37a37d22246f
15353453553 <albertzacher@posteo.de>
2023-05-02 20:35:06 +0200

    just some ideas

---
+++ database_operations.py
@@ -0,0 +1,32 @@
+import sqlite3
+
+class database_operations:
+
+    def __init__(self, name):
+        self.__db = sqlite3.connect(name)
+
+    def make_table(self, name, args: list):
+        c = self.__db.cursor()
+        params = ''
+
+        try:
+            for arg in args:
+                params = params + arg + ',\n'
+            c.execute('''
+                            CREATE TABLE records(
+                                id INTEGER PRIMARY KEY,
+                                name varchar(30),
+                                duration INTEGER,
+                                blocksize INTEGER,
+                                date DATE
+                            );
+                    ''')
+            self.__db.commit()
+
+        except Exception as e:
+            print(e)
+
+    def statement(self, statement: str):
+        c = self.__db.cursor()
+        c.execute(statement)
+        return c.fetchall()
---
+++ image_manager.py
@@ -0,0 +1,8 @@
+import database_operations as do
+
+class image_manager:
+    
+    def upload(pictures: list):
+        do.make_table('images', ['id INTEGER PRIMARY KEY', 'name varchar(30)'])
+
+        # ...
