692bd4d8e6cb42d7e2dfe20c9f2cceb38cf00764
15353453553 <albertzacher@posteo.de>
2023-05-03 10:55:47 +0200

    Kommentar

--- image_manager.py
+++ image_manager.py
@@ -5,4 +5,4 @@ class image_manager:
     def upload(pictures: list):
         do.make_table('images', ['id INTEGER PRIMARY KEY', 'name varchar(30)'])
 
-        # ...
+        # ... add virtual environment
