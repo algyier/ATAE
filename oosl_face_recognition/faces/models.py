01b2e3130dd4a5f74284cfd58ae7f398f323bebb
arneallwardt <fx.zype@gmail.com>
2023-05-11 10:44:04 +0200

    models und db

--- oosl_face_recognition/faces/models.py
+++ oosl_face_recognition/faces/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
+class Photographer(models.Model):
+    username = models.CharField(max_length=128)
+    key = models.CharField(max_length=128)
+
+
+class Picture(models.Model):
+    photographer = models.ForeignKey(Photographer, on_delete=models.SET_NULL, null=True)
+    file = models.ImageField(upload_to='files/pictures')
+
+
+class Face(models.Model):
+    file = models.ImageField(upload_to='files/faces')
+    pictures = models.ManyToManyField(Picture)
+
+
+
